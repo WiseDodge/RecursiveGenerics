947c3f4a96e5280b63782753822acd0c26771b7c
WiseDodge <115532049+WiseDodge@users.noreply.github.com>
2023-11-20 00:29:48 -0500

    Update RecursiveReverser.java

--- src/GenericRecursion/RecursiveReverser.java
+++ src/GenericRecursion/RecursiveReverser.java
@@ -1,2 +1,25 @@
+package GenericRecursion;
+
 public class RecursiveReverser {
+
+    public int reverse(int value) {
+        String reversedString = reverseString(Integer.toString(value));
+        return Integer.parseInt(reversedString);
+    }
+
+    public double reverse(double value) {
+        String reversedString = reverseString(Double.toString(value));
+        return Double.parseDouble(reversedString);
+    }
+
+    public String reverse(String value) {
+        return reverseString(value);
+    }
+
+    private String reverseString(String reversedString) {
+        if (reversedString == null || reversedString.length() <= 1) {
+            return reversedString;
+        }
+        return reverseString(reversedString.substring(1)) + reversedString.charAt(0);
+    }
 }
